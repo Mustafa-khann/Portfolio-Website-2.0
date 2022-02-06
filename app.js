9afa88854e3988462594bf974cd919b12edfb0a9
Mustafa <mkafridi073@gmail.com>
2022-02-06 13:50:02 +0500

    added a Node-Express REST api

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+
+// express server
+const app = express();
+const server = http.createServer(app);
+
+// server port
+const port = process.env.PORT || 3000;
+//staticfiles
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express API endpoints
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/html/index.html'));
+});
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/html/index.html'));
+});
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/html/about.html'));
+});
+app.get('/work', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/html/work.html'));
+});
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/html/contact.html'));
+});
+
+server.listen(port, function() {
+    console.log('Server started on port 3000');
+})
